b0207997f57c2ff052b694609fca959341c9aa45
CalGrimes <cal.grimes@outlook.com>
2025-01-13 23:53:09 +0000

    Switch to databricks

--- airflow-dbt-dag/dags/dbt_dag.py
+++ airflow-dbt-dag/dags/dbt_dag.py
@@ -2,17 +2,23 @@ import os
 from datetime import datetime
 
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
-from cosmos.profiles import SnowflakeUserPasswordProfileMapping
+from cosmos.profiles import DatabricksOauthProfileMapping
 
 profile_config = ProfileConfig(
     profile_name="dev",
     target_name="dev",
-    profile_mapping=SnowflakeUserPasswordProfileMapping(
-        conn_id="snowflake_conn", 
-        profile_args={"database": "subnational_consumption", "schema": "dbt_schema"},
+    profile_mapping=DatabricksOauthProfileMapping(
+        conn_id="databricks_conn", 
+        profile_args={
+            "host": os.getenv("DATABRICKS_HOST"), 
+            "schema": os.getenv("DATABRICKS_SCHEMA"), 
+            "http_path": os.getenv("DATABRICKS_HTTP_PATH"),
+            "client_id": os.getenv("DATABRICKS_CLIENT_ID"), 
+            "client_secret": os.getenv("DATABRICKS_CLIENT_SECRET")
+        }
     )
 )
-dbt_snowflake_dag = DbtDag(
+dbt_databricks_dag = DbtDag(
     project_config=ProjectConfig("/usr/local/airflow-dbt-dag/dags/dbt/dbt_consumption_pipeline",),
     operator_args={"install_deps": True},
     profile_config=profile_config,
