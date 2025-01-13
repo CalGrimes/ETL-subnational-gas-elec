05053a62bdc0ad28aebadc0969a9d0dcf5018810
CalGrimes <cal.grimes@outlook.com>
2025-01-13 22:01:20 +0000

    Switch to Databricks

--- dbt_consumption_pipeline/models/marts/fct_sn__postcode_consumption.sql
+++ dbt_consumption_pipeline/models/marts/fct_sn__postcode_consumption.sql
@@ -1,19 +1,19 @@
 select
   postcodes.postcode
-  ,postcodes."County Name" as county_name
-  ,postcodes."Region Name" as region_name
-  ,postcodes."Local Authority Name" as local_authority_name
-  ,consumption_summary."Electricity Total Consumption (kWh)"
-  ,consumption_summary."Gas Total Consumption (kWh)"
-  ,consumption_summary."Electricity Mean Consumption (kWh)"
-  ,consumption_summary."Gas Mean Consumption (kWh)"
-  ,consumption_summary."Electricity Median Consumption (kWh)"
-  ,consumption_summary."Gas Median Consumption (kWh)"
-  ,consumption_summary."analytics_year"
+  ,postcodes.`county_name` as county_name
+  ,postcodes.`region_name` as region_name
+  ,postcodes.`local_authority_name` as local_authority_name
+  ,consumption_summary.`electricity_total_consumption_kwh`
+  ,consumption_summary.`gas_total_consumption_kwh`
+  ,consumption_summary.`electricity_mean_consumption_kwh`
+  ,consumption_summary.`gas_mean_consumption_kwh`
+  ,consumption_summary.`electricity_median_consumption_kwh`
+  ,consumption_summary.`gas_median_consumption_kwh`
+  ,consumption_summary.`analytics_year`
 from
   {{ ref('stg_sn__postcodes') }} as postcodes  
 join
     {{ ref('int_sn__consumption_summary') }} as consumption_summary
 on 
-  postcodes.postcode = consumption_summary."Postcode"
-order by consumption_summary."analytics_year" desc
+  postcodes.postcode = consumption_summary.`postcode`
+order by consumption_summary.`analytics_year` desc
